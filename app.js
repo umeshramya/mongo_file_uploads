78b65e370f6c6299495ee9c096fe1844f3c60747
umeshramya <umeshbilagi@gmail.com>
2019-08-27 23:43:04 +0530

    commit

--- app.js
+++ app.js
@@ -148,6 +148,6 @@ app.delete('/files/:id', (req, res) => {
   });
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
